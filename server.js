3f913fe60de1b990f2d441c6a17eab854e7ce508
Bruno Krebs <bruno.krebs@auth0.com>
2018-06-10 19:00:13 -0300

    replacing express with koa

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
-const express = require('express');
+const Koa = require('koa');
 const next = require('next');
+const Router = require('koa-router');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const server = express();
+  const server = new Koa();
+  const router = new Router();
 
-  server.get('*', (req, res) => {
-    return handle(req, res);
+  router.get('/empreendimentos/:path', async ctx => {
+    const actualPage = '/building';
+    const queryParams = { path: ctx.params.path };
+    await app.render(ctx.req, ctx.res, actualPage, queryParams);
+    ctx.respond = false;
   });
+
+  router.get('*', async ctx => {
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+  });
+
+  server.use(async (ctx, next) => {
+    ctx.res.statusCode = 200;
+    await next();
+  });
+
+  server.use(router.routes());
 
   server.listen(3000, (err) => {
     if (err) throw err;
