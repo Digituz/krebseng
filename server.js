fa55f212d0ca99260c0ea4dae96fa8a07036055c
Bruno Krebs <bruno.krebs@auth0.com>
2018-06-11 19:46:05 -0300

    updating deps

--- server.js
+++ server.js
@@ -46,8 +46,8 @@ app.prepare().then(() => {
 
   server.listen(3000, (err) => {
     if (err) throw err;
-    console.log('> Ready on http://localhost:3000')
-  })
+    console.log('> Ready on http://localhost:3000');
+  });
 }).catch((ex) => {
   console.error(ex.stack);
   process.exit(1);
