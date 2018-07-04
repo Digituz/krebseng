e4ccc8a6561191a2c6f102f401dbfe5019a2258d
Bruno Krebs <bruno.krebs@auth0.com>
2018-07-04 10:45:29 -0300

    integrating with new version of Components.RestFlexRoute;

--- pages/panel/buildings.js
+++ pages/panel/buildings.js
@@ -1,17 +1,30 @@
 import * as Components from '@digituz/react-components';
+import {withRouter} from 'next/router';
 import Building from '../../entities/Building';
 import withPanelTemplate from '../../components/withPanelTemplate';
 import withUser from '../../components/withUser';
 
-function buildings(props) {
-  const tableColumns = ['buildingName'];
+const Buildings = (props) => {
+  const tableColumns = ['title'];
+  Building.url = 'http://localhost:3000/buildings';
+
   return (
     <Components.RestFlexRoute
       auth0Config={props.auth0Client.getProperties()}
       model={Building}
       tableColumns={tableColumns}
+      showList={!props.buildingId}
+      pushUrl={props.router.push}
     />
   );
-}
+};
 
-export default withUser(withPanelTemplate(buildings));
+Buildings.getInitialProps = async function(context) {
+  const { path } = context.query;
+
+  return {
+    buildingId: path,
+  }
+};
+
+export default withRouter(withUser(withPanelTemplate(Buildings)));
